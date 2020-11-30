cc606d0232689dc399a449f42814662603f2b2b5
JunSeobKim <wnstjq616@gmail.com>
2020-11-30 10:48:25 +0900

    Add alamofire and delegate textfield, pickerview

--- Calculator/ViewController/LoginViewController.swift
+++ Calculator/ViewController/LoginViewController.swift
@@ -6,13 +6,60 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var emailFieldForLoginVC: UITextField!
+    @IBOutlet weak var passwordFieldForLoginVC: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
+// func
+    func callCalculator() {
+        AF.request("https://api.spoonacular.com/food/trivia/random?apiKey=f78347a30c0e43a8be0fad4a9c491509").response { response in
+            debugPrint(response)
+            print(response)
+        }
     }
     
+    private func configureTextFields() {
+        emailFieldForLoginVC.delegate = self
+        passwordFieldForLoginVC.delegate = self
+    }
+    
+    private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap () {
+        view.endEditing(true)
+    }
+    
+// IBAction
+    @IBAction func loginTapped(_ sender: UIButton) {
+        print(emailFieldForLoginVC.text!)
+        print(passwordFieldForLoginVC.text!)
+        //TODO: error handling
+        view.endEditing(true)
+    }
+    
+// viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //callCalculator()
+        configureTextFields()
+        configureTapGesture()
+        
+
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
--- Calculator/ViewController/RegisterViewController.swift
+++ Calculator/ViewController/RegisterViewController.swift
@@ -7,23 +7,80 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let ageArray = Array(5...100)
+    
     @IBOutlet weak var popRegisterVC: UIButton!
+    @IBOutlet weak var emailFieldForRegisterVC: UITextField!
+    @IBOutlet weak var passwordFieldForRegisterVC: UITextField!
+    @IBOutlet weak var showAgePicker: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         navigationController?.isNavigationBarHidden = true
+        configureTextField()
+        createPickerView()
+        dismissPickerView()
+        
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        showAgePicker.tintColor = .clear
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+        
+    }
     
     @IBAction func popRegisterVC(_ sender: UIButton) {
-        
         navigationController?.popViewController(animated: true)
         
     }
     
+// func
+    private func configureTextField() {
+        emailFieldForRegisterVC.delegate = self
+        passwordFieldForRegisterVC.delegate = self
+        //agePickerForRegisterVC.delegate = self
+        //agePickerForRegisterVC.dataSource = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ageArray.count
+    }
+    internal func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(ageArray[row])
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        showAgePicker.text = String(ageArray[row])
+    }
+    func createPickerView() {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        showAgePicker.inputView = pickerView
+    }
+    func dismissPickerView() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let button = UIBarButtonItem(title: "선택", style: .plain, target: self, action: #selector(self.action))
+        toolBar.setItems([button], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        showAgePicker.inputAccessoryView = toolBar
+    }
+    @objc func action() {}
     
 }
 
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
